3d2227b18ecf1c1088076b7959aa7abb51eaa23b
Julian Gallegos <julianjgallegos1@gmail.com>
2022-10-04 23:38:09 +0000

    DELETE functionality implemented

---
+++ modules/deleteBooks.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const Book = require('../models/book');
+
+async function deleteBooks(req,res, next){
+	const id = req.params.id;
+    console.log(id);
+    try{
+		await Book.findByIdAndDelete(id);
+		res.status(204).send('Successfully Deleted');
+	}catch(error){
+		next(error);
+	}
+}
+
+module.exports = deleteBooks;
